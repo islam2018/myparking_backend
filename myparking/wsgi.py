82af63cf8908ec17c6d7bd8e795af89ef3df6d00
User <fm_bouayache@esi.dz>
2020-03-16 15:13:29 +0100

    configuring for prod

--- myparking/wsgi.py
+++ myparking/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myparking.prod.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myparking.prod_settings')
 
 
 application = get_wsgi_application()
