6e2ffa3222bc71340f8aa9d9818e493aee33a0c7
User <fm_bouayache@esi.dz>
2020-06-18 15:17:55 +0100

    changing db setting

--- myparking/settings.py
+++ myparking/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = 'myparking.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'djongo',
-    #     'NAME': 'heroku_0c5hh5pp',
-    #     'HOST': 'mongodb://parking:parking0>@ds113495.mlab.com:13495/heroku_0c5hh5pp?retryWrites=false',
-    #     'USER': 'parking',
-    #     'PASSWORD': 'parking0',
-    #     'TEST': {
-    #         'NAME': 'heroku_0c5hh5pp'
-    #     }
-    # },
-    # for simulations
     'default': {
         'ENGINE': 'djongo',
-        'NAME': 'heroku_rznd846m',
-        'HOST': 'mongodb://parking:parking0@ds045297.mlab.com:45297/heroku_rznd846m?retryWrites=false',
+        'NAME': 'heroku_0c5hh5pp',
+        'HOST': 'mongodb://parking:parking0>@ds113495.mlab.com:13495/heroku_0c5hh5pp?retryWrites=false',
         'USER': 'parking',
-        'PASSWORD': 'parking0'
+        'PASSWORD': 'parking0',
+        'TEST': {
+            'NAME': 'heroku_0c5hh5pp'
+        }
     },
+    # for simulations
+    # 'default': {
+    #     'ENGINE': 'djongo',
+    #     'NAME': 'heroku_rznd846m',
+    #     'HOST': 'mongodb://parking:parking0@ds045297.mlab.com:45297/heroku_rznd846m?retryWrites=false',
+    #     'USER': 'parking',
+    #     'PASSWORD': 'parking0'
+    # },
     # 'test': {
     #     'ENGINE': 'djongo',
     #     'NAME': 'heroku_rznd846m',
